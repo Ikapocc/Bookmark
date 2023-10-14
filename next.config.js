9bf8f5ee417262d95d95842dddcc063e937c3520
santiago <santiago348793@gmail.com>
2023-10-13 21:24:59 -0500

    minor fixes

--- next.config.js
+++ next.config.js
@@ -1,22 +1,17 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
   output: 'export',
+ 
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+ 
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+ 
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
 }
-
-module.exports = {
-  theme: {
-    extend: {
-      // Define tamaños de relleno personalizados
-      padding: {
-        '360px': '360px',
-      },
-    },
-    // Define colores personalizados
-    colors: {
-      'Soft-Blue': 'hsl(231, 69%, 60%)',
-      'Soft-Red': 'hsl(0, 94%, 66%)',
-      'Grayish-Blue': 'hsl(229, 8%, 60%)',
-      'Very-Dark-Blue': 'hsl(229, 31%, 21%)'
-    }
-  }
-}
+ 
+module.exports = nextConfig
