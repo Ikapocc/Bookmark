cc0ea2d767ea0215775e1108e9e82ed011447a58
santiago <santiago348793@gmail.com>
2023-10-13 21:20:44 -0500

    minor fixes

--- src/components/hamburger.jsx
+++ src/components/hamburger.jsx
@@ -14,7 +14,7 @@ export default function Hamburger({data}) {
         <>
             <nav className={`hidden max-md:grid ${isClicked ? "after:content-['*'] after:fixed after:inset-0 after:bg-gray-900 opacity-80" : ""}`}>
                 {isClicked ? <button className="z-10" onClick={() => setIsClicked(!isClicked)}><Image src={closed} alt="hamburger closed"></Image></button> : <button className="z-10" onClick={() => setIsClicked(!isClicked)}><Image src={open} alt="hamburger open"></Image></button>}
-                <ul className={isClicked ? "fixed flex flex-col text-xl text-center mx-auto z-10 top-20 inset-x-7 text-white cursor-pointer justify-between max-h-screen" : "hidden"}>
+                <ul className={isClicked ? "fixed flex flex-col text-xl text-center mx-auto z-10 top-20 inset-x-7 text-white cursor-pointer justify-between h-4/5" : "hidden"}>
                     <div className="grid gap-12 py-8 font-medium">
                         {data.map((items, idx) => (
                             <li key={idx}>{items}</li>
@@ -25,10 +25,10 @@ export default function Hamburger({data}) {
 
                     <ul className='flex gap-10 justify-center'>
                         <li>
-                        <Image src={facebook} alt='facebook-icon'/>
+                            <Image src={facebook} alt='facebook-icon'/>
                         </li>
                         <li>
-                        <Image src={twitter} alt='twitter-icon'/>
+                            <Image src={twitter} alt='twitter-icon'/>
                         </li>
                     </ul>
                 </ul>
